9e484faa41f2cf309073bc6685815c38948abc23
Summer T <summer@summertime-development.com>
2024-10-01 11:24:14 -0500

    cleanup

--- out/index.js
+++ out/index.js
@@ -1,101 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var chroma_console_1 = require("chroma-console");
-chroma_console_1.Coco.log('tests');
-/*
-const interface DataPoint
-{
-string name = "Amsterdam",
-float tempature = 50.5,
-float lat = 40.55,
-float long = 40.444
-
-}
-
-
-const interface FinalDataPointType
-{
-string name = "Amsterdam",
-float average_tempature = 30.5,
-float min_tempature = 10.5,
-float max_tempature = 50.5,
-
-}
-
-const billiowRows = [huge];
+chroma_console_1.Coco.start();
+var billionRows = [
+    { cityName: 'Amsterdam', tempature: 50.5 },
+    { cityName: 'Amsterdam', tempature: 51.5 },
+    { cityName: 'Amsterdam', tempature: 49.5 },
+];
 billionRows.sort(); // alphabet
-
-
-
-let finalObject : FinalDataPointType = [{}]
-let tempArray = [];
-
-
-
-const checkElement = (element : DataPoint) =>
-{
-
-if(tempArray.length === 0)
-{
-tempArray.push(element)
+// let finalObject : FinalDataPointType = [{}]
+var tempArray = [];
+var checkElement = function (element) {
+    if (tempArray.length === 0) {
+        tempArray.push(element);
+    }
+    else if (element.cityName !== tempArray[0].name) {
+        // new city
+        tempArray = [];
+        tempArray.push(element);
+    }
+    else {
+        if (element.tempature < tempArray[0].min_tempature) {
+            tempArray[0].min_tempature = element.tempature;
+        }
+        if (element.tempature > tempArray[0].max_tempature) {
+            tempArray[0].max_tempature = element.tempature;
+        }
+        // let total = 0
+        // for(int i  = 0; i < tempArray.length; i++)
+        // {
+        // total =+ tempArray.tempature;
+        // }
+        // tempArray[0].average_tempature = total / 3;
+    }
+};
+function main() {
+    var finalArray = [];
+    for (var i = 0; i < billionRows.length; i++) {
+        var returnElement = billionRows.map(function (x) { return checkElement(x); });
+        finalArray.push(returnElement);
+    }
+    chroma_console_1.Coco.log(finalArray);
 }
-else if (element.name !== tempArray[0].name) // new city
-{
-tempArray = [];
-tempArray.push(elment);
-}
-else
-{
-
-if(element.min_tempature < tempArray[0].min_tempature)
-{
-tempArray[0].min_tempature = element.min_tempature;
-}
-
-if(element.max_tempature > tempArray[0].max_tempature)
-{
-tempArray[0].max_tempature = element.max_tempature;
-}
-
-let total = 0
-for(int i  = 0; i < tempArray.length; i++)
-{
-total =+ tempArray.average_tempature;
-}
-
-tempArray[0].average_tempature = total / tempArraylength;
-
-
-
-}
-
-
-
-let finalyArray = [];
-
-
-for(const i; i < billion.rows.length, i++)
-{
-const returnElement = billionRows.map((x) => checkElemment())
-finalArray.push(returnElement);
-
-
-
-}
-
-*/
-function test() {
-    var array = [
-        41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86,
-        51,
-    ];
-    console.log(array.map(function (x) { return x * 2; }));
-    var test = function (x) { return x * 4; };
-    console.log(array.map(function (x) { return test(x); }));
-    console.log(array.filter(function (x) { return x % 2 === 0; }));
-    var reducer = function (accumulator, currentValue) {
-        return accumulator + currentValue;
-    };
-    console.log(array.reduce(reducer));
-}
-test();
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,108 +1,69 @@
 import { validateFxn } from './helpers.js';
 import { Coco } from 'chroma-console';
 
-Coco.log('tests');
-/*
-const interface DataPoint
-{
-string name = "Amsterdam",
-float tempature = 50.5,
-float lat = 40.55,
-float long = 40.444
+Coco.start();
 
+interface FinalDataPointType {
+    cityName: string;
+    dataPointCount: number;
+    tempature: number;
+    min_tempature: number;
+    max_tempature: number;
+    average_tempature: number;
 }
 
-
-const interface FinalDataPointType
-{
-string name = "Amsterdam",
-float average_tempature = 30.5,
-float min_tempature = 10.5,
-float max_tempature = 50.5,
-
+interface DataPoint {
+    cityName: string;
+    tempature: number;
 }
 
-const billiowRows = [huge];
+const billionRows: DataPoint[] = [
+    { cityName: 'Amsterdam', tempature: 50.5 },
+    { cityName: 'Amsterdam', tempature: 51.5 },
+    { cityName: 'Amsterdam', tempature: 49.5 },
+];
+
 billionRows.sort(); // alphabet
 
-
-
-let finalObject : FinalDataPointType = [{}]
+// let finalObject : FinalDataPointType = [{}]
 let tempArray = [];
 
+const checkElement = (element: DataPoint) => {
+    if (tempArray.length === 0) {
+        tempArray.push(element);
+    } else if (element.cityName !== tempArray[0].name) {
+        // new city
+        tempArray = [];
+        tempArray.push(element);
+    } else {
+        if (element.tempature < tempArray[0].min_tempature) {
+            tempArray[0].min_tempature = element.tempature;
+        }
 
+        if (element.tempature > tempArray[0].max_tempature) {
+            tempArray[0].max_tempature = element.tempature;
+        }
 
-const checkElement = (element : DataPoint) =>
-{
+        // let total = 0
+        // for(int i  = 0; i < tempArray.length; i++)
+        // {
+        // total =+ tempArray.tempature;
+        // }
 
-if(tempArray.length === 0)
-{
-tempArray.push(element)
-} 
-else if (element.name !== tempArray[0].name) // new city
-{
-tempArray = [];
-tempArray.push(elment);
-}
-else
-{
+        // tempArray[0].average_tempature = total / 3;
+    }
+};
 
-if(element.min_tempature < tempArray[0].min_tempature)
-{
-tempArray[0].min_tempature = element.min_tempature;
-}
+function main(): void {
+    let finalArray = [];
+    for (let i = 0; i < billionRows.length; i++) {
+        const returnElement = billionRows.map((x) => checkElement(x));
+        finalArray.push(returnElement);
+    }
 
-if(element.max_tempature > tempArray[0].max_tempature)
-{
-tempArray[0].max_tempature = element.max_tempature;
-}
-
-let total = 0
-for(int i  = 0; i < tempArray.length; i++)
-{
-total =+ tempArray.average_tempature;
-}
-
-tempArray[0].average_tempature = total / tempArraylength;
-
-
-
+    Coco.log(finalArray);
 }
 
-
-
-let finalyArray = [];
-
-
-for(const i; i < billion.rows.length, i++)
-{
-const returnElement = billionRows.map((x) => checkElemment())
-finalArray.push(returnElement);
-
-
-
-}
-
-*/
-
-function test(): void {
-    const array = [
-        41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86,
-        51,
-    ];
-
-    console.log(array.map((x) => x * 2));
-
-    const test = (x: number) => x * 4;
-    console.log(array.map((x) => test(x)));
-
-    console.log(array.filter((x) => x % 2 === 0));
-
-    const reducer = (accumulator: number, currentValue: number) =>
-        accumulator + currentValue;
-    console.log(array.reduce(reducer));
-}
-
-test();
+main();
 
 export {};
