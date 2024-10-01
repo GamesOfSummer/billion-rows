b47967fc661e1467da2d8ebf758db2ea13216def
Summer T <summer@summertime-development.com>
2024-10-01 10:44:39 -0500

    fiddling with the settings

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,22 +1,3 @@
-export const consoleStart = () => {
-    console.log('\x1b[33m', 'Running test cases...');
-    console.log('');
-    console.log('');
-};
-
-export const consoleEnd = () => {
-    console.log('');
-    console.log('');
-    console.log('\x1b[33m', 'End of test cases...');
-    console.log('\x1b[37m', '');
-    console.log('');
-};
-
-export const consoleBuffer = () => {
-    console.log('--------------------------');
-    console.log('--------------------------');
-    console.log('\n');
-};
 
 export const validateFxn = (output: any, answer: any) => {
     //console.log('\x1b[36m%s\x1b[0m', '🛠 Testing function...');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,89 @@
-import {
-    consoleBuffer,
-    consoleEnd,
-    consoleStart,
-    validateFxn,
-} from './helpers.js';
+import { validateFxn } from './helpers.js';
+import { Coco } from 'chroma-console';
+
+Coco.log('tests');
+/*
+const interface DataPoint
+{
+string name = "Amsterdam",
+float tempature = 50.5,
+float lat = 40.55,
+float long = 40.444
+
+}
+
+
+const interface FinalDataPointType
+{
+string name = "Amsterdam",
+float average_tempature = 30.5,
+float min_tempature = 10.5,
+float max_tempature = 50.5,
+
+}
+
+const billiowRows = [huge];
+billionRows.sort(); // alphabet
+
+
+
+let finalObject : FinalDataPointType = [{}]
+let tempArray = [];
+
+
+
+const checkElement = (element : DataPoint) =>
+{
+
+if(tempArray.length === 0)
+{
+tempArray.push(element)
+} 
+else if (element.name !== tempArray[0].name) // new city
+{
+tempArray = [];
+tempArray.push(elment);
+}
+else
+{
+
+if(element.min_tempature < tempArray[0].min_tempature)
+{
+tempArray[0].min_tempature = element.min_tempature;
+}
+
+if(element.max_tempature > tempArray[0].max_tempature)
+{
+tempArray[0].max_tempature = element.max_tempature;
+}
+
+let total = 0
+for(int i  = 0; i < tempArray.length; i++)
+{
+total =+ tempArray.average_tempature;
+}
+
+tempArray[0].average_tempature = total / tempArraylength;
+
+
+
+}
+
+
+
+let finalyArray = [];
+
+
+for(const i; i < billion.rows.length, i++)
+{
+const returnElement = billionRows.map((x) => checkElemment())
+finalArray.push(returnElement);
+
+
+
+}
+
+*/
 
 function test(): void {
     const array = [
@@ -23,9 +103,6 @@ function test(): void {
     console.log(array.reduce(reducer));
 }
 
-consoleStart();
 test();
-
-consoleEnd();
 
 export {};
