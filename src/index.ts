6ab9ca6b96551bf16f9a2dab3e3327e58a76a34e
Summer T <summer@summertime-development.com>
2024-10-03 09:55:18 -0500

    this

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { validateFxn } from './helpers.js';
 import { Coco } from 'chroma-console';
 
+Coco.setColor('firewood');
 Coco.start();
 
 interface FinalDataPointType {
@@ -12,6 +13,15 @@ interface FinalDataPointType {
     average_tempature: number;
 }
 
+const emptyCity: FinalDataPointType = {
+    cityName: '',
+    dataPointCount: 0,
+    tempature: 0,
+    min_tempature: 0,
+    max_tempature: 0,
+    average_tempature: 0,
+};
+
 interface DataPoint {
     cityName: string;
     tempature: number;
@@ -21,6 +31,8 @@ const billionRows: DataPoint[] = [
     { cityName: 'Amsterdam', tempature: 50.5 },
     { cityName: 'Amsterdam', tempature: 51.5 },
     { cityName: 'Amsterdam', tempature: 49.5 },
+    { cityName: 'Austria', tempature: 49.5 },
+    { cityName: 'Austria', tempature: 44.5 },
 ];
 
 billionRows.sort(); // alphabet
@@ -28,40 +40,51 @@ billionRows.sort(); // alphabet
 // let finalObject : FinalDataPointType = [{}]
 let tempArray = [];
 
-const checkElement = (element: DataPoint) => {
-    if (tempArray.length === 0) {
-        tempArray.push(element);
-    } else if (element.cityName !== tempArray[0].name) {
-        // new city
-        tempArray = [];
-        tempArray.push(element);
-    } else {
-        if (element.tempature < tempArray[0].min_tempature) {
-            tempArray[0].min_tempature = element.tempature;
-        }
-
-        if (element.tempature > tempArray[0].max_tempature) {
-            tempArray[0].max_tempature = element.tempature;
-        }
-
-        // let total = 0
-        // for(int i  = 0; i < tempArray.length; i++)
-        // {
-        // total =+ tempArray.tempature;
-        // }
-
-        // tempArray[0].average_tempature = total / 3;
+const checkElement = (accumulator: FinalDataPointType, element: DataPoint) => {
+    if (element.tempature < tempArray[0].min_tempature) {
+        accumulator.min_tempature = element.tempature;
     }
+
+    if (element.tempature > tempArray[0].max_tempature) {
+        accumulator.max_tempature = element.tempature;
+    }
+
+    // let total = 0
+    // for(int i  = 0; i < tempArray.length; i++)
+    // {
+    // total =+ tempArray.tempature;
+    // }
+
+    // tempArray[0].average_tempature = total / 3;
+
+    return accumulator;
 };
 
 function main(): void {
-    let finalArray = [];
-    for (let i = 0; i < billionRows.length; i++) {
-        const returnElement = billionRows.map((x) => checkElement(x));
-        finalArray.push(returnElement);
-    }
+    let finalArray: FinalDataPointType[] = [];
 
-    Coco.log(finalArray);
+    let currentCity = '';
+    for (let i = 0; i < billionRows.length; i++) {
+        if (currentCity !== billionRows[i].cityName) {
+            finalArray.push({
+                cityName: billionRows[i].cityName,
+                ...emptyCity,
+            });
+        }
+
+        const allCityDataPoints = billionRows.filter(
+            (x) => x.cityName === currentCity
+        );
+
+        const accumulator: FinalDataPointType = emptyCity;
+        const returnElement = allCityDataPoints.reduce(
+            checkElement(accumulator, x),
+            accumulator
+        );
+
+        Coco.log(returnElement);
+        //finalArray.push(returnElement);
+    }
 }
 
 main();
